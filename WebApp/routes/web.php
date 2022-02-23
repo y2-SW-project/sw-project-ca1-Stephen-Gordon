6e9a7cda3ca5039efa06c1283e354481226f8bef
Stephen-Gordon <n00202054@iadt.ie>
2022-02-23 15:06:05 +0000

    Started creating index

--- WebApp/routes/web.php
+++ WebApp/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\User\PostController as UserPostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -25,3 +25,8 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('admin/home', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('admin.home');
 
 Route::get('user/home', [App\Http\Controllers\User\HomeController::class, 'index'])->name('user.home');
+
+
+//Index 
+Route::get('user/posts/', [UserPostController::class, 'index'])->name('user.posts.index');
+Route::get('user/posts/{id}', [UserPostController::class, 'show'])->name('user.posts.show');
